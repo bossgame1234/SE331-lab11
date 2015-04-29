1fbf898e2640f1a6df1bf4493d73b84f70e72baa
Dto <chartchai.d@cmu.ac.th>
2015-04-29 09:41:54 +0700

    first commit

---
+++ src/main/webapp/views/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var labApp = angular.module('labApp', [
+    'ngRoute',
+    'productMainController',
+    'languageControllers',
+    'languageServices',
+    'pascalprecht.translate',
+    'shoppingCartControllers',
+    'flow'
+])
+labApp.config(['$routeProvider',
+  function($routeProvider) {
+  $routeProvider.
+      when('/addProduct',{
+          templateUrl: 'template/editProduct.html',
+          controller: 'addProductController'
+      }).
+      when('/editProduct/:id',{
+          templateUrl: 'template/editProduct.html',
+          controller: 'editProductController'
+      }).
+      when('/listProduct',{
+          templateUrl: 'template/productList.html',
+          controller: 'listProductController'
+      }).
+      when('/shoppingCart/:id',{
+          templateUrl: 'template/shoppingCart.html',
+          controller: 'showShoppingCartController'
+      }).
+       otherwise({redirectTo: '/listProduct'});
+}]);
+
+labApp.config(function($translateProvider){
+    $translateProvider.useUrlLoader('/messageBundle');
+    $translateProvider.useStorage('UrlLanguageStorage');
+    $translateProvider.preferredLanguage('en');
+    $translateProvider.fallbackLanguage('en');
+})
+
+labApp.config(['flowFactoryProvider', function (flowFactoryProvider) {
+    flowFactoryProvider.defaults = {
+        target: '',
+        permanentErrors: [ 500, 501],
+        maxChunkRetries: 1,
+        chunkRetryInterval: 5000,
+        simultaneousUploads: 4,
+        singleFile: false
+    };
+    flowFactoryProvider.on('catchAll', function (event) {
+        console.log('catchAll', arguments);
+    });
+    // Can be used with different implementations of Flow.js
+    // flowFactoryProvider.factory = fustyFlowFactory;
+}]);
